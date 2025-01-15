4906ced4c4a291cf667cf209fc7a8ec753b0651f
xxcb4000 <vincent.bonhomme@gmail.com>
2025-01-15 18:24:17 +0100

    Formulaire opérationnel !

---
+++ js/formulaire.js
@@ -0,0 +1,25 @@
+const boutonFormulaire = document.getElementById('buttonValidate');
+
+
+boutonFormulaire.onclick = function onclickFormulaire() {
+    let formData = {
+        prenom : document.getElementById('prénom').value,
+        nom : document.getElementById('nom').value,
+        email : document.getElementById('email').value,
+        message : document.getElementById('message').value,
+        coche : document.getElementById('news').value,
+    };
+
+    emailjs.send("service_zwug8sj", "template_zgkhkp6", formData)
+    .then(() => {
+        alert("Formulaire envoyé avec succès !");
+    })
+    .catch((error) => {
+        console.error("Erreur lors de l'envoi :", error);
+        alert("Une erreur est survenue lors de l'envoi du formulaire.");
+    });
+
+
+
+
+}
